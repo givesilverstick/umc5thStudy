ab55c7148510ebabae7cebaabb1e05f4e28421a3
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-11-19 11:53:40 +0900

    여러 엔티티들 생성

---
+++ src/main/java/umc/spring/domain/mapping/MemberPrefer.java
@@ -0,0 +1,20 @@
+package umc.spring.domain.mapping;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberPrefer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
